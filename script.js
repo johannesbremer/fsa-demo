3ac0a2707bdef3a240326db9639a1a3757e24e90
Glitch (glitch-hello-website) <none>
2023-10-04 20:52:41 +0000

    🌷💄 Checkpoint ./script.js:43197774/719 ./index.html:43197774/241

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-async function openFilePicker() {
 
-  const fileHandles = await window.showOpenFilePicker();
+
+async function openFilePicker() {
+  const fileHandles = await window.showOpenFilePicker({});
   insertHandleElement(fileHandles[0]);
-}
+};
+
+async function saveFilePicker() {
+  const fileHandles = await window.showSaveFilePicker();
+  insertHandleElement(fileHandles[0]);
+};
+
+async function directoryPicker() {
+  const dirHandle = await window.showDirectoryPicker();
+  insertHandleElement(dirHandle);
+};
 
 function insertHandleElement() {
+  const containerElem = document.getElementById('handle-container');
   
-}
+};
