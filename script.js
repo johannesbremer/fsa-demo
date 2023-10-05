cd98fbb6c85cd9cac0ad849a4e5c7f554d45e509
Glitch (glitch-hello-website) <none>
2023-10-05 16:18:27 +0000

    🛥⛺️ Checkpoint ./index.html:43197774/144 ./script.js:43197774/2971

--- script.js
+++ script.js
@@ -26,26 +26,39 @@ function insertHandleElement(handle) {
   handleType.innerText = handle.kind;
   row.append(handleType);
   
-  const permission = document.createElement('td');
-  permission.innerText = '-';
-  row.append(permission);
-  
-  const actionButtons = document.createElement('td');
+  const readPermission = document.createElement('td');
   const queryReadPermissionButton = document.createElement('button');
-  queryReadPermissionButton.innerText = "Query Read Access";
-  const queryReadWritePermissionButton = document.createElement('button');
-  queryReadWritePermissionButton.innerText = "Query ReadWrite Access";
+  queryReadPermissionButton.innerText = 'Query';
   const requestReadPermissionButton = document.createElement('button');
-  requestReadPermissionButton.innerText = "Request Read Access";
+  requestReadPermissionButton.innerText = 'Request';
+  const readPermissionState = document.createElement('span');
+  readPermissionState.innerText = '-';
+  readPermission.append(queryReadPermissionButton);
+  readPermission.append(requestReadPermissionButton);
+  readPermission.append(readPermissionState);
+  row.append(readPermission);
+  
+  const readWritePermission = document.createElement('td');
+  const queryReadWritePermissionButton = document.createElement('button');
+  queryReadWritePermissionButton.innerText = 'Query';
   const requestReadWritePermissionButton = document.createElement('button');
-  requestReadWritePermissionButton.innerText = "Request ReadWrite Access";
+  requestReadWritePermissionButton.innerText = 'Request';
+  const readWritePermissionState = document.createElement('span');
+  readWritePermissionState.innerText = '   -   ';
+  readWritePermission.append(queryReadWritePermissionButton);
+  readWritePermission.append(requestReadWritePermissionButton);
+  readWritePermission.append(readWritePermissionState);
+  row.append(readWritePermission);
   
-  actionButtons.append(queryReadPermissionButton);
-  actionButtons.append(queryReadWritePermissionButton);
-  actionButtons.append(requestReadPermissionButton);
-  actionButtons.append(requestReadWritePermissionButton);
-  row.append(actionButtons);
+  const indexedDB = document.createElement('td');
+  const saveButton = document.createElement('button');
+  saveButton.innerText = 'Save to IndexedDB';
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'Remove';
+  indexedDB.append(saveButton);
+  indexedDB.append(removeButton);
+  row.append(indexedDB);
   
-  const table = document.getElementById('handle-table');  
+  const table = document.getElementById('handle-table'); 
   table.appendChild(row);
 };
